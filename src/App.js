ad76dde632e11b7680d3d9db1a922fbe1bc1b94a
yuminc03 <yuminc03@gamil.com>
2021-07-20 15:57:01 +0900

    2021-07-20: 블로그 글 세개 쓰고 좋아요 버튼 누르면 올라가기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  // let posts = ['즐거운 React시간! ✨', 'JavaScript를 배웁시다 🔔', 'Node.js 시간입니다 🎇'];
+  // let days = ["5월 16일 발행", "7월 11일 발행", "7월 20일 발행"];
+  let [title, setTitle] = useState(['즐거운 React시간! ✨', 'JavaScript를 배웁시다 🔔', 'Node.js 시간입니다 🎇']);
+  let [day, setDay] = useState(['5월 16일 발행', '7월 11일 발행', '7월 20일 발행']);
+  //let posts = '스포츠 관련';
+
+  let [likeBtn, setlikeBtn] = useState(0);
+
+  const increase = () => {
+    setlikeBtn(likeBtn + 1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>Yumin's Blog</div>
+      </div>
+      <div className="list">
+        <h3>{title[0]}</h3><span onClick={increase}>👍</span> {likeBtn}
+        <p>{day[0]}</p>
+        <hr/>
+        <h3>{title[1]}</h3><span onClick={increase}>👍</span> {likeBtn}
+        <p>{day[1]}</p>
+        <hr/>
+        <h3>{title[2]}</h3><span onClick={increase}>👍</span> {likeBtn}
+        <p>{day[2]}</p>
+        {/* <h3>{posts}</h3>
+        <p>2월 17일 발행</p> */}
+        <hr/>
+      </div>
     </div>
   );
 }
