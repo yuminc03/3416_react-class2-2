f75cba21cfa00fdd59bece29b75d07e8718c1fca
yuminc03 <yuminc03@gmail.com>
2021-07-21 15:55:09 +0900

    2021-07-21: 블로그의 첫번째 글 제목 수정하는 버튼 만들기

--- src/App.js
+++ src/App.js
@@ -8,10 +8,18 @@ function App() {
   let [day, setDay] = useState(['5월 16일 발행', '7월 11일 발행', '7월 20일 발행']);
   //let posts = '스포츠 관련';
 
-  let [likeBtn, setlikeBtn] = useState(0);
+  let [likeBtn, setlikeBtn] = useState(0);//좋아요 버튼
 
   const increase = () => {
     setlikeBtn(likeBtn + 1);
+  }
+
+  const updateTitle = () => {
+    var newTitle = [...title];
+    newTitle[0] = "즐겁고 유익한 React시간!💎";
+    setTitle(newTitle);
+    //state는 불변의 요소라서 상태를 직접 바꾸어서는 안됨
+    //title[0] = 'React';
   }
   return (
     <div className="App">
@@ -19,6 +27,7 @@ function App() {
         <div>Yumin's Blog</div>
       </div>
       <div className="list">
+        <button onClick={updateTitle}>수정 버튼</button>
         <h3>{title[0]}</h3><span onClick={increase}>👍</span> {likeBtn}
         <p>{day[0]}</p>
         <hr/>
